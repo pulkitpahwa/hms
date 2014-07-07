4ec7626444a2baa796dbef7e510f37c4a5f966a7
pulkitpahwa <pulkitpahwa11@gmail.com>
2014-07-07 23:46:45 +0530

    Attendance.models documented and more fields added

--- hms/attendance/models.py
+++ hms/attendance/models.py
@@ -12,14 +12,27 @@ class Outpass(models.Model) :
             ('Y','Yes'),
             ('N','No'),
         )
+    outpass_choice = (
+            ('W','Waiting'),
+            ('P','Processed'),
+        )
+    save_choice = (
+            ('SMS', 'SMS'),
+            ('PDF','PDF'),
+        )
     enrollment_id  = models.OneToOneField(Student)
     outpass_id = models.AutoField(primary_key = True)
     from_time = models.DateTimeField('Out Time')
     return_time = models.DateTimeField('Expected Return')
-    permission_required = models.CharField(max_length = 1, choices = permission_choice)
-    permission_granted_by = models.ManyToManyField(Staff, blank = True, null = True)
+    permission_required = models.CharField(max_length = 1, choices = permission_choice) #will be Y if the outpass is not requested on a holiday
+    permission_granted_by = models.ManyToManyField(Staff, blank = True, null = True) # will keep a check by whom the permission is granted i.e by which teacher
     permission_granted_by_hostel_staff = models.ManyToManyField(HostelStaff, blank = True, null = True)
-    
+    outpass_state = models.CharField(max_length = 2, choices = outpass_choice) #checks whether the output is in waiting state or is generated completely
+    outpass_generated = models.DateTimeField("Outpass generated at" ) # fill it in views.py file. WHen the form is completely filled and the outpass could be generated i.e. permission is not required, then outpass_generated = true
+    outpass_save_mode = models.CharField(max_length = 3, choices = save_choice) #checks how the outpass is saved i.e. SMS or pdf
+
+#fields to be filled by the user :from_time, to_time 
+#fields to be auto-filled in views.py : enrollment_id, outpass_id, permission_required, permission_granted_by, permission_granted_by_hostel_staff, outpass_generated, outpass_save_mode
 
 class Attendance(models.Model):
     present_choice = (
