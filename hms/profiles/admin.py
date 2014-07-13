5c7755593a81f6267fefff2c8aa121c26a5ee104
pulkitpahwa <pulkitpahwa11@gmail.com>
2014-07-13 23:40:59 +0530

    admin updated

--- hms/profiles/admin.py
+++ hms/profiles/admin.py
@@ -2,22 +2,21 @@ from django.contrib import admin
 from .models import Student, Staff, HostelStaff
 
 class StudentAdmin(admin.ModelAdmin):
-#    list_display=('enrollment_id', 'name','course','batch','father_number','city')
-#    list_filter = ['course', 'batch']
-#    search_fields = ['enrollment_id', 'name', 'course','city','country']
-    pass
+    list_display=('enrollment_id', 'name','course','batch','father_number','city')
+    list_filter = ['course', 'batch']
+    search_fields = ['enrollment_id', 'name', 'course','city','country']
 
 class StaffAdmin(admin.ModelAdmin):
-#    list_display=('staff_id', 'name','branch1','contact')
-#    list_filter = ['name', 'branch1']
-#    search_fields = ['staff_id', 'name', 'branch1']
-    pass
+    list_display=('enrollment_id', 'name','branch1','contact')
+    list_filter = ['name', 'branch1']
+    search_fields = ['enrollment_id', 'name', 'branch1']
 
 class HostelStaffAdmin(admin.ModelAdmin):
-#    list_display=('staff_id', 'name','position','contact')
-#    list_filter = ['name', 'position']
-#    search_fields = ['staff_id', 'name', 'position']
-    pass
+    list_display=('enrollment_id', 'name','position','contact')
+    list_filter = ['name', 'position']
+    search_fields = ['enrollment_id', 'name', 'position']
+
+
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Staff, StaffAdmin)
 admin.site.register(HostelStaff, HostelStaffAdmin)
